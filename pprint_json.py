a700af8d3c61947ead74510903d9d77594d6b6fe
danaconda <danaconda666@gmail.com>
2017-10-09 16:40:31 -0400

    pep8 fixes

--- pprint_json.py
+++ pprint_json.py
@@ -3,10 +3,10 @@ import sys
 import os
 
 
-
 def load_data(file_path):
     with open(file_path, 'r') as file_handler:
         return json.load(file_handler)
+
 
 def pretty_print_json(json_data):
     print(json.dumps(json_data, sort_keys=True, indent=4))
